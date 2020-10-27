ef261efc25a56c3a06a4186f5513593b30d4ef21
Kenta000 <kenta.malaysia@gmail.com>
2020-10-27 17:26:46 +0800

    up

--- database/coTracker.sql
+++ database/coTracker.sql
@@ -1,2 +1,11 @@
 create database CoTracker;
 use CoTracker;
+
+create table users(
+username varchar(20),
+password varchar(20),
+name varchar(30),
+position varchar(15),  -- either Patient,Tester, Manager
+patientType varchar(15),
+symptoms varchar(30),
+);
